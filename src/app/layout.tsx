3ba0ab5066390641a99181baa2d4cd6685c4e03f
Joshua Stone <joshualeestone@users.noreply.github.com>
2025-09-09 20:44:15 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css';
+import "./globals.css";
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-black text-white">{children}</body>
     </html>
-  )
+  );
 }
